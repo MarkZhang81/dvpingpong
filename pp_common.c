MarkZhang81/dvpingpong

commit beb134f0ed9bfa84feba12cb7b7bb38218596227

dvpingpong: Init version
This is a RDMA client/server application example.
This patch add verbs API support, meaning it works over verbs API.

pp_common.c
@@ -1,0 +1,260 @@
+#include "pp_common.h"
+
+static inline char *get_link_layer_str(int layer)
+{
+	if (layer == IBV_LINK_LAYER_INFINIBAND)
+		return "infiniband";
+	else if (layer == IBV_LINK_LAYER_ETHERNET)
+		return "ethernet";
+	else
+		return "unspecified";
+
+}
+
+static int pp_open_ibvdevice(const char *ibv_devname, struct pp_context *ppctx)
+{
+	int i = 0, ret = 0;
+	const char *devname;
+	struct ibv_device **dev_list;
+
+	dev_list = ibv_get_device_list(NULL);
+	if (!dev_list) {
+		ERR("ibv_get_device_list()");
+		return -errno;
+	}
+
+	for (i = 0; dev_list[i] != NULL; i++) {
+		devname = ibv_get_device_name(dev_list[i]);
+		//printf("=DEBUG:%s:%d: %s, %s\n", __func__, __LINE__, dev, s_ibv_devname);
+		if (strncmp(devname, ibv_devname, strlen(ibv_devname)) == 0) {
+			INFO("Device found: %d/%s\n", i, devname);
+			break;
+		}
+	}
+	if (dev_list[i] == NULL) {
+		ERR("Device not found: %d/%s\n", i, ibv_devname);
+		return -1;
+	}
+
+	ppctx->dev_list = dev_list;
+	ppctx->ibctx = ibv_open_device(dev_list[i]);
+	if (ppctx->ibctx == NULL) {
+		ERR("ibv_open_device(%s), i=%d", ibv_devname, i);
+		return errno;
+	}
+
+	ppctx->port_num = PORT_NUM;
+	do {
+		ret = ibv_query_port(ppctx->ibctx, PORT_NUM, &ppctx->port_attr);
+		if (ret) {
+			perror("ibv_query_port");
+			return ret;
+		}
+		INFO("ibdev %s port %d port_state %d (expect %d) phy_state %d\n", ibv_devname, ppctx->port_num,
+		     ppctx->port_attr.state, IBV_PORT_ACTIVE, ppctx->port_attr.phys_state);
+
+		if (ppctx->port_attr.state == IBV_PORT_ACTIVE)
+			break;
+		sleep(1);
+	} while (1);
+
+	INFO("ibdev %s port %d lid %d state %d, mtu max %d active %d, link_layer %d(%s) phy_state %d speed %d\n",
+	     ibv_devname, ppctx->port_num, ppctx->port_attr.lid, ppctx->port_attr.state,
+	     ppctx->port_attr.max_mtu, ppctx->port_attr.active_mtu,
+	     ppctx->port_attr.link_layer, get_link_layer_str(ppctx->port_attr.link_layer),
+	     ppctx->port_attr.phys_state, ppctx->port_attr.active_speed);
+
+	/*
+        if (ppctx->port_attr.link_layer != IBV_LINK_LAYER_ETHERNET) {
+		server_sgid_idx = 0;
+		client_sgid_idx = 0;
+	}
+	*/
+	return 0;
+}
+
+static void pp_close_ibvdevice(struct ibv_context *ibctx)
+{
+	int err = ibv_close_device(ibctx);
+	if (err)
+		perror("mz_close_ibvdevice");
+}
+
+
+int pp_ctx_init(struct pp_context *pp, const char *ibv_devname,
+		int use_vfio, const char *vfio_pci_name)
+{
+	struct mlx5dv_vfio_context_attr vfio_ctx_attr = {
+		.pci_name = vfio_pci_name,
+		.flags = MLX5DV_VFIO_CTX_FLAGS_INIT_LINK_DOWN,
+		.comp_mask = 0,
+	};
+	int ret, i;
+
+	pp->cap.max_send_wr = PP_MAX_WR;
+	pp->cap.max_recv_wr = PP_MAX_WR;
+	pp->cap.max_send_sge = 1;
+	pp->cap.max_recv_sge = 1;
+	pp->cap.max_inline_data = 64;
+
+	if (use_vfio) {
+		struct ibv_device *ibdev;
+
+		pp->dev_list = mlx5dv_get_vfio_device_list(&vfio_ctx_attr);
+		if (!pp->dev_list) {
+			ERR("mlx5dv_get_vfio_device_list returns NULL\n");
+			return errno;
+		}
+		ibdev = pp->dev_list[0];
+		pp->ibctx = ibv_open_device(ibdev);
+		if (!pp->ibctx) {
+			ERR("ibv_open_device(%s) failed: %d\n", vfio_pci_name, errno);
+			return errno;
+		}
+		pp->port_num = PORT_NUM;
+	} else {
+		ret = pp_open_ibvdevice(ibv_devname, pp);
+		if (ret)
+			return ret;
+	}
+
+	pp->pd = ibv_alloc_pd(pp->ibctx);
+	if (!pp->pd) {
+		ERR("ibv_alloc_pd() failed\n");
+		ret = errno;
+		goto fail_alloc_pd;
+	}
+
+	pp->mrbuflen = 1 << 20;
+	for (i = 0; i < PP_MAX_WR; i++) {
+		pp->mrbuf[i] = memalign(sysconf(_SC_PAGESIZE), pp->mrbuflen);
+		if (!pp->mrbuf[i]) {
+			ERR("%d: memalign(0x%lx) failed\n", i, pp->mrbuflen);
+			ret = errno;
+			goto fail_memalign;
+		}
+
+		pp->mr[i] = ibv_reg_mr(pp->pd, pp->mrbuf[i], pp->mrbuflen,
+				       IBV_ACCESS_LOCAL_WRITE | IBV_ACCESS_REMOTE_READ | IBV_ACCESS_REMOTE_WRITE);
+		if (!pp->mr[i]) {
+			ERR("%d: ibv_reg_mr() failed\n", i);
+			ret = errno;
+			goto fail_reg_mr;
+		}
+	}
+
+	if (use_vfio)
+		INFO("VFIO open(%s) succeeds, flags 0x%x\n\n", vfio_pci_name, vfio_ctx_attr.flags);
+	else
+		INFO("Initialization succeeds(regular)\n\n");
+
+
+	return 0;
+
+fail_reg_mr:
+	for (i = 0; i < PP_MAX_WR; i++)
+		if (pp->mr[i])
+			ibv_dereg_mr(pp->mr[i]);
+fail_memalign:
+	for (i = 0; i < PP_MAX_WR; i++)
+		free(pp->mrbuf[i]);
+	ibv_dealloc_pd(pp->pd);
+fail_alloc_pd:
+	pp_close_ibvdevice(pp->ibctx);
+
+	return ret;
+}
+
+void pp_ctx_cleanup(struct pp_context *pp)
+{
+	int i;
+
+	for (i = 0; i < PP_MAX_WR; i++)
+		ibv_dereg_mr(pp->mr[i]);
+	for (i = 0; i < PP_MAX_WR; i++)
+		free(pp->mrbuf[i]);
+	ibv_dealloc_pd(pp->pd);
+	pp_close_ibvdevice(pp->ibctx);
+}
+
+static void print_gid(struct pp_context *ppc, unsigned char *p)
+{
+	if (ppc->port_attr.link_layer != IBV_LINK_LAYER_ETHERNET)
+		return;
+
+	printf("                                    gid %02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x:%02x\n",
+	       p[0], p[1], p[2], p[3], p[4], p[5], p[6], p[7],
+	       p[8], p[9], p[10], p[11], p[12], p[13], p[14], p[15]);
+}
+
+
+extern int sock_client(const char *server_ip, char *sendbuf, int send_buflen,
+		       char *recvbuf, int recv_buflen);
+extern int sock_server(char *sendbuf, int send_buflen, char *recvbuf, int recv_buflen);
+
+/* %sip NULL means local is server, otherwise local is client */
+int pp_exchange_info(struct pp_context *ppc, int my_sgid_idx,
+		     int my_qp_num, uint32_t my_psn,
+		     struct pp_exchange_info *remote, const char *sip)
+{
+	char sendbuf[1024] = {}, recvbuf[1024] = {};
+	struct pp_exchange_info *local = (struct pp_exchange_info *)sendbuf;
+	struct pp_exchange_info *r = (struct pp_exchange_info *)recvbuf;
+	unsigned char *p;
+	int ret, i;
+
+	if (!ppc->port_num) {
+		ERR("pp_context isn't initialized: pp->port_num is 0\n");
+		return -1;
+	}
+
+	if (ppc->port_attr.link_layer == IBV_LINK_LAYER_ETHERNET) {
+		ret = ibv_query_gid(ppc->ibctx, ppc->port_num,
+				    my_sgid_idx, &local->gid);
+		if (ret) {
+			ERR("ibv_query_gid failed %d\n", ret);
+			return ret;
+		}
+	} else {
+		local->lid = htobe32(ppc->port_attr.lid);
+	}
+
+	local->qpn = htobe32(my_qp_num);
+	local->psn = htobe32(my_psn);
+	for (i = 0; i < PP_MAX_WR; i++) {
+		local->addr[i] = (void *)htobe64((uint64_t)ppc->mrbuf[i]);
+		local->mrkey[i] = htobe32(ppc->mr[i]->lkey);
+	}
+	p = local->gid.raw;
+	INFO("Local(%s): port_num %d, lid %d, psn 0x%x, qpn 0x%x(%d), addr %p, mrkey 0x%x\n",
+	     sip ? "Client" : "Server", ppc->port_num, ppc->port_attr.lid, my_psn,
+	     my_qp_num, my_qp_num, ppc->mrbuf[0], ppc->mr[0]->lkey);
+	print_gid(ppc, p);
+
+	if (sip)
+		ret = sock_client(sip, sendbuf, sizeof(*local),
+				  recvbuf, sizeof(recvbuf));
+	else
+		ret = sock_server(sendbuf, sizeof(*local),
+				  recvbuf, sizeof(recvbuf));
+	if (ret) {
+		ERR("socket failed %d, server_ip %s\n", ret, sip ? sip : "");
+		return ret;
+	}
+
+	remote->qpn = be32toh(r->qpn);
+	remote->psn = be32toh(r->psn);
+	for (i = 0; i < PP_MAX_WR; i++) {
+		remote->addr[i] = (void *)be64toh((uint64_t)r->addr[i]);
+		remote->mrkey[i] = be32toh(r->mrkey[i]);
+	}
+	memcpy(&remote->gid, &r->gid, sizeof(remote->gid));
+	p = remote->gid.raw;
+	INFO("Remote(%s): lid %d, psn 0x%x, qpn 0x%x(%d), addr %p, mrkey 0x%x\n",
+	     sip ? "Server" : "Client", remote->lid, remote->psn,
+	     remote->qpn, remote->qpn, remote->addr[i], remote->mrkey[0]);
+	print_gid(ppc, p);
+	printf("\n");
+
+	return 0;
+}

Makefile
@@ -1,0 +1,22 @@
+CC := gcc
+LD := gcc
+CFLAGS := -Wall -g -O0 -I.
+
+LIBS :=  -libverbs -lmlx5  -lpthread
+
+HEADERS := pp_common.h
+OBJS_VERB := sock.o pp_common.o pp_verb.o
+
+all: server client.verb
+
+server: server.o $(OBJS_VERB)
+	$(LD) -o $@ $^ $(LIBS)
+
+client.verb: client.verb.o $(OBJS_VERB)
+	$(LD) -o $@ $^ $(LIBS)
+
+%.o: %.c $(HEADERS)
+	$(CC) $(CFLAGS) -c $< -o $@
+
+clean:
+	rm -f server client.verb *.o

client.verb.c
@@ -1,0 +1,98 @@
+#include "pp_common.h"
+#include "pp_verb.h"
+
+//#define SERVER_IP "10.237.1.205"
+#define SERVER_IP "192.168.0.122"
+
+static char ibv_devname[100] = "rocep8s0f0";
+static int client_sgid_idx = 3;
+
+#define PP_VERB_OPCODE_CLIENT IBV_WR_RDMA_WRITE_WITH_IMM /* IBV_WR_SEND_WITH_IMM */
+
+#define PP_SEND_WRID_CLIENT  0x1000
+#define PP_RECV_WRID_CLIENT  0x4000
+
+static struct pp_verb_ctx ppv_ctx;
+static struct pp_exchange_info server = {};
+
+static int client_traffic_verb(struct pp_verb_ctx *ppv)
+{
+	struct ibv_send_wr wrs[PP_MAX_WR] = {}, *bad_wr_send;
+	struct ibv_recv_wr wrr[PP_MAX_WR] = {}, *bad_wr_recv;
+	struct ibv_sge sglists[PP_MAX_WR] = {};
+	int max_wr_num = PP_MAX_WR, ret;
+
+	DBG("Pause 1sec ");
+	sleep(1);		/* Wait until server side is ready to recv */
+	DBG("Do post_send %d messages with length 0x%lx..\n", max_wr_num, ppv->ppc.mrbuflen);
+
+	prepare_send_wr_verb(ppv, wrs, sglists, &server, max_wr_num,
+			     PP_SEND_WRID_CLIENT, PP_VERB_OPCODE_CLIENT, true);
+
+	/* 1. Send "ping" */
+	ret = ibv_post_send(ppv->cqqp.qp, wrs, &bad_wr_send);
+	if (ret) {
+		ERR("%d: ibv_post_send failed %d\n", max_wr_num, ret);
+		return ret;
+	}
+
+	ret = poll_cq_verb(ppv, max_wr_num, false);
+	if (ret) {
+		ERR("poll_cq_verb failed\n");
+		return ret;
+	}
+
+	INFO("Send done, now recving reply...\n");
+	prepare_recv_wr_verb(ppv, wrr, sglists, max_wr_num, PP_RECV_WRID_CLIENT);
+	/* 2. Get "pong" with same data */
+	ret = ibv_post_recv(ppv->cqqp.qp, wrr, &bad_wr_recv);
+	if (ret) {
+		ERR("%d: ibv_post_send failed %d\n", max_wr_num, ret);
+		return ret;
+	}
+
+	ret = poll_cq_verb(ppv, max_wr_num, true);
+	if (ret) {
+		ERR("poll_cq_verb failed\n");
+		return ret;
+	}
+
+	INFO("Client(verb) traffic test done\n");
+	return 0;
+}
+
+int main(int argc, char *argv[])
+{
+	int ret;
+
+	if (argv[1]) {
+		memset(ibv_devname, 0, sizeof(ibv_devname));
+		strcpy(ibv_devname, argv[1]);
+	}
+
+	ret = pp_ctx_init(&ppv_ctx.ppc, ibv_devname, 0, NULL);
+	if (ret)
+		return ret;
+
+	ret = pp_create_cq_qp_verb(&ppv_ctx.ppc, &ppv_ctx.cqqp);
+	if (ret)
+		goto out_create_cq_qp;
+
+	ret = pp_exchange_info(&ppv_ctx.ppc, client_sgid_idx, ppv_ctx.cqqp.qp->qp_num,
+			       CLIENT_PSN, &server, SERVER_IP);
+	if (ret)
+		goto out_exchange;
+
+	ret = pp_move2rts_verb(&ppv_ctx.ppc, ppv_ctx.cqqp.qp, client_sgid_idx,
+			       CLIENT_PSN, &server);
+	if (ret)
+		goto out_exchange;
+
+	ret = client_traffic_verb(&ppv_ctx);
+
+out_exchange:
+	pp_destroy_cq_qp_verb(&ppv_ctx.cqqp);
+out_create_cq_qp:
+	pp_ctx_cleanup(&ppv_ctx.ppc);
+	return ret;
+}

pp_common.h
@@ -1,0 +1,89 @@
+#ifndef _PP_COMMON_H
+#define _PP_COMMON_H
+
+#include <errno.h>
+#include <malloc.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <infiniband/verbs.h>
+#include <infiniband/mlx5dv.h>
+
+#define DBG(fmt, args...) \
+	printf("=MZDBG:%s:%d: " fmt, __func__, __LINE__, ##args)
+
+#define INFO(fmt, args...) \
+	printf("=MZINFO:%s:%d: " fmt, __func__, __LINE__, ##args)
+
+#define ERR(fmt, args...) \
+	fprintf(stderr, "=MZERR:%s:%d(%d:%s) " fmt, __func__, __LINE__, errno, strerror(errno), ##args)
+
+#define SERVER_PSN 0x1000
+#define CLIENT_PSN 0x8000
+
+#define PORT_NUM 1
+
+#define PP_MAX_WR 16		/* Max outstanding send/recv wr */
+
+#define PP_MAX_LOG_CQ_SIZE 8	/* 256 cqe */
+
+struct pp_context {
+	int port_num;
+	struct ibv_port_attr port_attr;
+	/* Same qp_cap for all apps, so that they'll have same behavior */
+	struct ibv_qp_cap cap;
+
+	struct ibv_device **dev_list;
+	struct ibv_context *ibctx;
+	struct ibv_pd *pd;
+
+	struct ibv_mr *mr[PP_MAX_WR];
+	unsigned char *mrbuf[PP_MAX_WR];
+	ssize_t mrbuflen;
+};
+
+struct pp_exchange_info {
+	uint32_t qpn;
+	uint32_t psn;
+
+	union ibv_gid gid;
+	uint32_t lid; /* For IB only */
+
+	/* For RDMA read/write */
+	void *addr[PP_MAX_WR];
+	uint32_t mrkey[PP_MAX_WR];
+};
+
+int pp_ctx_init(struct pp_context *pp, const char *ibv_devname,
+		int use_vfio, const char *vfio_pci_name);
+void pp_ctx_cleanup(struct pp_context *pp);
+
+int pp_exchange_info(struct pp_context *ppc, int my_sgid_idx,
+		     int my_qp_num, uint32_t my_psn,
+		     struct pp_exchange_info *remote, const char *sip);
+
+static inline void mem_string(unsigned char *p, ssize_t len)
+{
+	ssize_t i;
+
+	for (i = 0; i < len - 1; i++)
+		p[i] = 'A' + i % ('z' - 'A' + 1);
+
+	p[len - 1] = '\0';
+}
+
+/* To dump a long string like this: "0: 0BCDEFGHIJKLMNOP ... nopqrstuvwxyABC" */
+static inline void dump_msg_short(int index, struct pp_context *ppc)
+{
+	ppc->mrbuf[index][ppc->mrbuflen - 1] = '\0';
+	if (ppc->mrbuflen <= 32) {
+		printf("    %2d: %s\n", index, ppc->mrbuf[index]);
+	} else {
+		ppc->mrbuf[index][16] = '\0';
+		printf("    %2d (len = 0x%lx): %s...%s\n", index, ppc->mrbuflen,
+		       ppc->mrbuf[index], ppc->mrbuf[index] + ppc->mrbuflen - 16);
+	}
+}
+#endif	/* _PP_COMMON_H */

pp_verb.c
@@ -1,0 +1,224 @@
+#include "pp_verb.h"
+
+int pp_create_cq_qp_verb(const struct pp_context *ppctx,
+			 struct pp_verb_cq_qp *ppv)
+{
+	int ret;
+
+	struct ibv_cq_init_attr_ex cq_init_attr_ex = {
+		.cqe = (1 << PP_MAX_LOG_CQ_SIZE) - 1,
+		.cq_context = NULL,
+		.channel = NULL,
+		.comp_vector = 0,
+	};
+	ppv->cq_ex = ibv_create_cq_ex(ppctx->ibctx, &cq_init_attr_ex);
+	if (!ppv->cq_ex) {
+		ERR("ibv_create_cq_ex() failed");
+		return errno;
+	}
+
+	struct ibv_qp_init_attr_ex init_attr = {
+		.sq_sig_all = 1,
+		.send_cq = ibv_cq_ex_to_cq(ppv->cq_ex),
+		.recv_cq = ibv_cq_ex_to_cq(ppv->cq_ex),
+		.cap = ppctx->cap,
+
+		.qp_type = IBV_QPT_RC,
+		.comp_mask = IBV_QP_INIT_ATTR_PD,
+		.pd = ppctx->pd,
+	};
+	ppv->qp = ibv_create_qp_ex(ppctx->ibctx, &init_attr);
+	if (!ppv->qp) {
+		ERR("ibv_create_qp_ex() failed");
+		ret = errno;
+		goto fail_create_qp;
+	}
+
+	struct ibv_qp_attr attr = {
+		.qp_state = IBV_QPS_INIT,
+		.pkey_index = 0,
+		.port_num = ppctx->port_num,
+		.qp_access_flags = IBV_ACCESS_LOCAL_WRITE | IBV_ACCESS_REMOTE_WRITE | IBV_ACCESS_REMOTE_READ,
+	};
+	ret = ibv_modify_qp(ppv->qp, &attr,
+			    IBV_QP_STATE | IBV_QP_PKEY_INDEX | IBV_QP_PORT | IBV_QP_ACCESS_FLAGS);
+	if (ret) {
+		perror("ibv_modify_qp");
+		goto fail_modify_qp;
+	}
+
+	return 0;
+
+fail_modify_qp:
+	ibv_destroy_qp(ppv->qp);
+fail_create_qp:
+	ibv_destroy_cq(ibv_cq_ex_to_cq(ppv->cq_ex));
+
+	return ret;
+}
+
+void pp_destroy_cq_qp_verb(struct pp_verb_cq_qp *ppv)
+{
+	ibv_destroy_qp(ppv->qp);
+	ibv_destroy_cq(ibv_cq_ex_to_cq(ppv->cq_ex));
+}
+
+int pp_move2rts_verb(struct pp_context *ppc, struct ibv_qp *qp,
+		     int my_sgid_idx, uint32_t my_sq_psn,
+		     struct pp_exchange_info *peer)
+{
+	int ret;
+
+	struct ibv_qp_attr attr = {
+		.qp_state		= IBV_QPS_RTR,
+		.path_mtu		= IBV_MTU_1024,
+		.dest_qp_num		= peer->qpn,
+		.rq_psn			= peer->psn,
+		.max_dest_rd_atomic	= 1,
+		.min_rnr_timer		= 12,
+		.ah_attr		= {
+			.is_global	= 0,
+			.src_path_bits	= 0,
+			.port_num	= ppc->port_num,
+		}
+	};
+
+	if (ppc->port_attr.link_layer == IBV_LINK_LAYER_ETHERNET) {
+		attr.ah_attr.is_global = 1;
+		attr.ah_attr.grh.hop_limit = 64;
+		attr.ah_attr.grh.sgid_index = my_sgid_idx;
+		attr.ah_attr.grh.dgid = peer->gid;
+	} else {
+		attr.ah_attr.dlid = peer->lid;
+	}
+
+	ret = ibv_modify_qp(qp, &attr,
+			    IBV_QP_STATE              |
+			    IBV_QP_AV                 |
+			    IBV_QP_PATH_MTU           |
+			    IBV_QP_DEST_QPN           |
+			    IBV_QP_RQ_PSN             |
+			    IBV_QP_MAX_DEST_RD_ATOMIC |
+			    IBV_QP_MIN_RNR_TIMER);
+	if (ret) {
+		ERR("Failed to modify to RTR: ret %d errno %d", ret, errno);
+		return ret;
+	}
+
+	attr.qp_state	    = IBV_QPS_RTS;
+	attr.timeout	    = 12;
+	attr.retry_cnt	    = 3;
+	attr.rnr_retry	    = 3;
+	attr.sq_psn	    = my_sq_psn;
+	attr.max_rd_atomic  = 1;
+	ret = ibv_modify_qp(qp, &attr,
+			    IBV_QP_STATE              |
+			    IBV_QP_TIMEOUT            |
+			    IBV_QP_RETRY_CNT          |
+			    IBV_QP_RNR_RETRY          |
+			    IBV_QP_SQ_PSN             |
+			    IBV_QP_MAX_QP_RD_ATOMIC);
+	if (ret) {
+		ERR("Failed to modify to RTR: ret %d errno %d", ret, errno);
+		return ret;
+	}
+
+	INFO("Local qp 0x%x(%d) moved to RTS state\n", qp->qp_num, qp->qp_num);
+	return 0;
+}
+
+void prepare_recv_wr_verb(struct pp_verb_ctx *ppv, struct ibv_recv_wr wrr[],
+			  struct ibv_sge sglists[], int max_wr_num,
+			  uint64_t wr_id)
+{
+	int i;
+
+	for (i = 0; i < max_wr_num; i++) {
+		memset(ppv->ppc.mrbuf[i], 0, ppv->ppc.mrbuflen);
+
+		sglists[i].lkey = ppv->ppc.mr[i]->lkey;
+		sglists[i].addr = (uint64_t)ppv->ppc.mrbuf[i];
+		sglists[i].length = ppv->ppc.mrbuflen;
+
+		if (i < max_wr_num - 1)
+			wrr[i].next = &wrr[i+1];
+		else
+			wrr[i].next = NULL;
+
+		wrr[i].wr_id = wr_id + i;
+		wrr[i].sg_list = &sglists[i];
+		wrr[i].num_sge = 1;
+	}
+}
+
+void prepare_send_wr_verb(struct pp_verb_ctx *ppv, struct ibv_send_wr wrs[],
+			  struct ibv_sge sglists[], struct pp_exchange_info *peer,
+			  int max_wr_num, uint64_t wr_id, int opcode, bool initbuf)
+{
+	int i;
+
+	for (i = 0; i < max_wr_num; i++) {
+		if (initbuf) {
+			mem_string(ppv->ppc.mrbuf[i], ppv->ppc.mrbuflen);
+			*ppv->ppc.mrbuf[i] = i % 16 + '0';
+		}
+
+		sglists[i].lkey = ppv->ppc.mr[i]->lkey;
+		sglists[i].addr = (uint64_t)ppv->ppc.mrbuf[i];
+		sglists[i].length = ppv->ppc.mrbuflen;
+
+		if (i < max_wr_num - 1)
+			wrs[i].next = &wrs[i+1];
+		else
+			wrs[i].next = NULL;
+
+		wrs[i].wr_id = wr_id + i;
+		wrs[i].sg_list = &sglists[i];
+		wrs[i].num_sge = 1;
+		wrs[i].imm_data = 0x10203040 + i;
+		wrs[i].opcode = opcode;
+		wrs[i].send_flags = IBV_SEND_SIGNALED;
+
+		if (wrs[i].opcode == IBV_WR_RDMA_WRITE_WITH_IMM) {
+			wrs[i].wr.rdma.remote_addr = (uint64_t)peer->addr[i];
+			wrs[i].wr.rdma.rkey = peer->mrkey[i];
+		}
+	}
+}
+
+int poll_cq_verb(struct pp_verb_ctx *ppv, int max_wr_num, bool for_recv)
+{
+	struct ibv_wc wcs[PP_MAX_WR];
+	int cq_recved = 0, cqn, i;
+
+	do {
+		do {
+			cqn = ibv_poll_cq(ibv_cq_ex_to_cq(ppv->cqqp.cq_ex),
+					  max_wr_num - cq_recved, wcs);
+			usleep(1000 * 10);
+		} while (cqn == 0);
+
+		if (cqn < 0) {
+			ERR("ibv_poll_cq failed, %d/%d\n", cq_recved, max_wr_num);
+			return -1;
+		}
+
+		for (i = 0; i < cqn; i++) {
+			if (wcs[i].status != IBV_WC_SUCCESS) {
+				ERR("Failed status %s(%x) for wr_id 0x%x\n",
+				    ibv_wc_status_str(wcs[i].status), wcs[i].status,
+				    (int)wcs[i].wr_id);
+				return -1;
+			}
+		}
+		if (for_recv)
+			for (i = 0; i < cqn; i++)
+				dump_msg_short(cq_recved + i, &ppv->ppc);
+		else
+			INFO("Polled %d/%d CQEs for post_send..\n", cq_recved + cqn, max_wr_num);
+
+		cq_recved += cqn;
+	} while (cq_recved < max_wr_num);
+
+	return 0;
+}

pp_verb.h
@@ -1,0 +1,28 @@
+#include "pp_common.h"
+
+struct pp_verb_cq_qp {
+	struct ibv_cq_ex *cq_ex;
+	struct ibv_qp *qp;
+};
+
+struct pp_verb_ctx {
+	struct pp_context ppc;
+	struct pp_verb_cq_qp cqqp;
+};
+
+int pp_create_cq_qp_verb(const struct pp_context *ppctx,
+			 struct pp_verb_cq_qp *ppv);
+void pp_destroy_cq_qp_verb(struct pp_verb_cq_qp *ppv);
+
+int pp_move2rts_verb(struct pp_context *ppc, struct ibv_qp *qp,
+		     int my_sgid_idx, uint32_t my_sq_psn,
+		     struct pp_exchange_info *peer);
+
+void prepare_recv_wr_verb(struct pp_verb_ctx *ppv, struct ibv_recv_wr wrr[],
+			  struct ibv_sge sglists[], int max_wr_num,
+			  uint64_t wr_id);
+void prepare_send_wr_verb(struct pp_verb_ctx *ppv, struct ibv_send_wr wrs[],
+			  struct ibv_sge sglists[], struct pp_exchange_info *peer,
+			  int max_wr_num, uint64_t wr_id, int opcode, bool initbuf);
+
+int poll_cq_verb(struct pp_verb_ctx *ppv, int max_wr_num, bool for_recv);

server.c
@@ -1,0 +1,92 @@
+#include "pp_common.h"
+#include "pp_verb.h"
+
+static char ibv_devname[100] = "rocep8s0f0";
+static int server_sgid_idx = 3;
+
+static struct pp_verb_ctx ppv_ctx;
+static struct pp_exchange_info client = {};
+
+#define PP_SEND_WRID_SERVER  0x10000000
+#define PP_RECV_WRID_SERVER  0x20000000
+
+#define PP_VERB_OPCODE_SERVER IBV_WR_RDMA_WRITE_WITH_IMM /* IBV_WR_SEND_WITH_IMM */
+
+static int server_traffic_verb(struct pp_verb_ctx *ppv)
+{
+	struct ibv_send_wr wrs[PP_MAX_WR] = {}, *bad_wrs;
+	struct ibv_recv_wr wrr[PP_MAX_WR] = {}, *bad_wrr;
+	struct ibv_sge sglists[PP_MAX_WR] = {};
+	int max_wr_num = PP_MAX_WR, ret;
+
+	prepare_recv_wr_verb(ppv, wrr, sglists, max_wr_num, PP_RECV_WRID_SERVER);
+
+	INFO("Waiting for data...\n");
+	/* 1. Recv "ping" */
+	ret = ibv_post_recv(ppv->cqqp.qp, wrr, &bad_wrr);
+	if (ret) {
+		ERR("%d: ibv_post_send failed %d\n", max_wr_num, ret);
+		return ret;
+	}
+
+	ret = poll_cq_verb(ppv, max_wr_num, true);
+	if (ret) {
+		ERR("poll_cq_verb failed\n");
+		return ret;
+	}
+
+	sleep(1);
+	INFO("Now sending reply...\n");
+	prepare_send_wr_verb(ppv, wrs, sglists, &client, max_wr_num,
+			     PP_SEND_WRID_SERVER, PP_VERB_OPCODE_SERVER, false);
+	ret = ibv_post_send(ppv->cqqp.qp, wrs, &bad_wrs);
+	if (ret) {
+		ERR("%d: ibv_post_send failed %d\n", max_wr_num, ret);
+		return ret;
+	}
+
+	ret = poll_cq_verb(ppv, max_wr_num, false);
+	if (ret) {
+		ERR("poll_cq_verb failed\n");
+		return ret;
+	}
+
+	INFO("Server traffic test done\n");
+	return 0;
+}
+
+int main(int argc, char *argv[])
+{
+	int ret;
+
+	if (argv[1]) {
+		memset(ibv_devname, 0, sizeof(ibv_devname));
+		strcpy(ibv_devname, argv[1]);
+	}
+
+	ret = pp_ctx_init(&ppv_ctx.ppc, ibv_devname, 0, NULL);
+	if (ret)
+		return ret;
+
+	ret = pp_create_cq_qp_verb(&ppv_ctx.ppc, &ppv_ctx.cqqp);
+	if (ret)
+		goto out_create_cq_qp;
+
+	ret = pp_exchange_info(&ppv_ctx.ppc, server_sgid_idx,
+			       ppv_ctx.cqqp.qp->qp_num, SERVER_PSN, &client, NULL);
+	if (ret)
+		goto out_exchange;
+
+	ret = pp_move2rts_verb(&ppv_ctx.ppc, ppv_ctx.cqqp.qp, server_sgid_idx,
+			       SERVER_PSN, &client);
+	if (ret)
+		goto out_exchange;
+
+	ret = server_traffic_verb(&ppv_ctx);
+
+out_exchange:
+	pp_destroy_cq_qp_verb(&ppv_ctx.cqqp);
+out_create_cq_qp:
+	pp_ctx_cleanup(&ppv_ctx.ppc);
+	return ret;
+}

sock.c
@@ -1,0 +1,115 @@
+// Server side C/C++ program to demonstrate Socket programming
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <errno.h>
+
+#define PORT 8668
+
+int sock_server(char *sendbuf, int send_buflen, char *recvbuf, int recv_buflen)
+{
+	int server_fd, new_socket, sent, readlen;
+	struct sockaddr_in address;
+	int opt = 1;
+	int addrlen = sizeof(address);
+	//char buffer[1024] = { 0 };
+	//char *hello = "Hello from server";
+
+	// Creating socket file descriptor
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+		perror("socket failed");
+		exit(EXIT_FAILURE);
+	}
+	// Forcefully attaching socket to the port 8080
+	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+		       &opt, sizeof(opt))) {
+		perror("setsockopt");
+		exit(EXIT_FAILURE);
+	}
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons(PORT);
+
+	// Forcefully attaching socket to the port 8080
+	if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+		perror("bind failed");
+		exit(EXIT_FAILURE);
+	}
+	if (listen(server_fd, 3) < 0) {
+		perror("listen");
+		exit(EXIT_FAILURE);
+ 	}
+	printf("    =DEBUG:%s:%d: waiting for client:\n", __func__, __LINE__);
+	if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+				 (socklen_t *) & addrlen)) < 0) {
+		perror("accept");
+		exit(EXIT_FAILURE);
+	}
+
+	readlen = read(new_socket, recvbuf, recv_buflen);
+	if (readlen <= 0) {
+		printf("=DEBUG:%s:%d: read %d expected %d errno %d\n", __func__, __LINE__,
+		       readlen, recv_buflen, errno);
+		return -1;
+	}
+
+	sent = send(new_socket, sendbuf, send_buflen, 0);
+	if (sent < 0) {
+		printf("=DEBUG:%s:%d: sent %d expected %d errno %d\n", __func__, __LINE__,
+		       sent, send_buflen, 0);
+		return -1;
+	}
+
+	//printf("    =DEBUG:%s:%d: Server info exchange done(recv_buflen %d readlen %d send_buflen %d sent %d)\n", __func__, __LINE__, recv_buflen, readlen, send_buflen, sent);
+	return 0;
+}
+
+int sock_client(const char *server_ip, char *sendbuf, int send_buflen,
+		char *recvbuf, int recv_buflen)
+{
+	//struct sockaddr_in address;
+	int sock = 0, sent, readlen;
+	struct sockaddr_in serv_addr;
+
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		printf("\n Socket creation error \n");
+		return -1;
+	}
+
+	memset(&serv_addr, '0', sizeof(serv_addr));
+
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(PORT);
+
+	// Convert IPv4 and IPv6 addresses from text to binary form
+	if (inet_pton(AF_INET, server_ip, &serv_addr.sin_addr) <= 0) {
+		printf("\nInvalid address/ Address not supported \n");
+		return -1;
+	}
+
+	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+		printf("\nConnection Failed \n");
+		return -1;
+	}
+
+	sent = send(sock, sendbuf, send_buflen, 0);
+	if (sent <= 0) {
+		printf("=DEBUG:%s:%d: sent %d expected %d errno %d\n", __func__, __LINE__,
+		       sent, send_buflen, 0);
+		return -1;
+	}
+
+	readlen = read(sock, recvbuf, recv_buflen);
+	if (readlen <= 0) {
+		printf("=DEBUG:%s:%d: read %d expected %d errno %d\n", __func__, __LINE__,
+		       readlen, recv_buflen, errno);
+		return -1;
+	}
+
+	//printf("    =DEBUG:%s:%d: Client info exchange done(recv_buflen %d readlen %d send_buflen %d sent %d)\n", __func__, __LINE__, recv_buflen, readlen, send_buflen, sent);
+	return 0;
+}

commit 5ece24987c4167daca8005612dad6d8d0b159e25

verb: Read lid from info-exchange message
The remote->lid need to be set during info-exchange stage.

Fixes: beb134f0ed9b ("dvpingpong: Init version")
Signed-off-by: Mark Zhang <markzhang@nvidia.com>

pp_common.c
@@ -241,6 +241,7 @@ int pp_exchange_info(struct pp_context *ppc, int my_sgid_idx,
 		return ret;
 	}
 
+	remote->lid = be32toh(r->lid);
 	remote->qpn = be32toh(r->qpn);
 	remote->psn = be32toh(r->psn);
 	for (i = 0; i < PP_MAX_WR; i++) {